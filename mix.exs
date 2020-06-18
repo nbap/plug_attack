85b4ef76d9fc1b40a83e85ede14226f54faf46da
Nicolas Baptista <nicolas@nbap.com.br>
2020-06-18 19:58:56 -0300

    Bump Elixir and Plug versions to 1.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugAttack.Mixfile do
   def project do
     [app: :plug_attack,
      version: "0.4.2",
-     elixir: "~> 1.4",
+     elixir: "~> 1.10",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
@@ -23,7 +23,7 @@ defmodule PlugAttack.Mixfile do
   end
 
   defp deps do
-    [{:plug, "~> 1.0"},
+    [{:plug, "~> 1.10"},
      {:ex_doc, "~> 0.19", only: :dev}]
   end
 
